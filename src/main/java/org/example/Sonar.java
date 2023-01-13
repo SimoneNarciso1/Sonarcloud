a8335f366e2e3f295de56f1860768b847656d403
simonenarciso <simonarciso95@gmail.com>
2023-01-13 19:05:45 +0100

    Initial commit teerdtry
    Signed-off-by: simonenarciso <simonarciso95@gmail.com>

--- src/main/java/org/example/Sonar.java
+++ src/main/java/org/example/Sonar.java
@@ -3,7 +3,7 @@ package org.example;
 class Sonar {
     public  void sum (int numero ){
             numero = numero + numero;
-            System.out.println(numero);
+
         }
 
         public static void main( String[] args){
